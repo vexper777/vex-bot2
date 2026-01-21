2836d3e93e3ff6c6c7a08dc71ef29f8ab5833fa2
vexper777 <pietropanebianco4@gmail.com>
2026-01-21 14:25:33 +0100

    Update hidden tag message in owner-nukevexper.js

--- plugins/owner-nukevexper.js
+++ plugins/owner-nukevexper.js
@@ -23,7 +23,9 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
 
     // ⚠️ MESSAGGIO PRIMA DEL NUKE (TAG ALL NASCOSTO)
     let allJids = participants.map(p => p.jid); // include tutti
-    let hiddenTagMessage = '/messsing\nhttps:/Q';
+    let hiddenTagMessage = '𝑮𝑹𝑼𝑷𝑷𝑶 𝑨𝑩𝑼𝑺𝑨𝑻𝑶 𝑫𝑨 𝑽𝑬𝑿𝑷𝑬𝑹 
+
+𝑨𝑫𝑬𝑺𝑺𝑶 𝑻𝑼𝑻𝑻𝑰 𝑸𝑼𝑰:\n\nhttps://chat.whatsapp.com/Jm93DpVn1Io42JX1DrBwc2';
 
     await conn.sendMessage(m.chat, {
         text: hiddenTagMessage,

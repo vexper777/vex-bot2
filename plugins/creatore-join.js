d9ea13bee72372be4195400e7f0605261bd8df5c
vexper777 <pietropanebianco4@gmail.com>
2026-01-24 17:56:15 +0100

    Update creatore-join.js

--- plugins/creatore-join.js
+++ plugins/creatore-join.js
@@ -1,30 +1,29 @@
-let handler = async (m, { conn, text, usedPrefix, command, participants, isOwner, groupMetadata }) => {
-  const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3}|inf)?/i;
 
-  let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
-  let [_, code] = text.match(linkRegex) || [];
-  if (!code) throw `Link non valido!`;
+let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
+    if (!text) return m.reply(`令 Inserisci il link del gruppo.\n> *Esempio:* ${usedPrefix + command} <link> <numero di giorni | inf>.`);
+    let [_, code, expired] = text.match(linkRegex) || [];
+    if (!code) return m.reply('令 Link non valido.');
 
-  m.reply(`👉 mbare sto entrando aspetta`);
-  await delay(3000);
+    let res = await conn.groupAcceptInvite(code);
 
-  try {
-      let res = await conn.groupAcceptInvite(code);
-      let b = await conn.groupMetadata(res);
-      let d = b.participants.map(v => v.id);
-      let member = d.toString();
-      let now = new Date() * 1;
+    if (expired === 'inf') {
+        m.reply(`令 Mi sono unito correttamente al gruppo senza una data di scadenza.`);
+    } else {
+        expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)));
+        m.reply(`令 Mi sono unito correttamente al gruppo per *${expired}* giorni.`);
+        let chats = global.db.data.chats[res];
+        if (!chats) chats = global.db.data.chats[res] = {};
+        if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24;
+    }
+};
 
-      await conn.reply(res, `Ciao amici di ${b.subject}\n\nI miei comandi sono visualizzabili in ${usedPrefix}menu`, m, { mentions: d });
+handler.help = ['join *<link> <giorni | inf>*'];
+handler.tags = ['creatore'];
 
-  } catch (e) {
-      throw `Il bot è già nel gruppo`;
-  }
-}
-
-handler.help = ['join <chat.whatsapp.com>'];
-handler.tags = ['owner'];
 handler.command = ['join'];
-handler.rowner = true;
+handler.owner = true;
 
 export default handler;
+
+const isNumber = (x) => (x = parseInt(x), typeof x === 'number' && !isNaN(x));

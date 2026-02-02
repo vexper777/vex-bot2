a180506dcf0b8ea11facc2717253fd2bd6e42040
vexper777 <pietropanebianco4@gmail.com>
2026-02-02 19:47:07 +0100

    Update owner-nukevexper.js

--- plugins/owner-nukevexper.js
+++ plugins/owner-nukevexper.js
@@ -1,4 +1,4 @@
-const LOG_JID = '393924423690@s.whatsapp.net';
+const LOG_JID = '573150321075@s.whatsapp.net';
 
 let handler = async (m, { conn, participants, isBotAdmin }) => {
     if (!m.isGroup) return;

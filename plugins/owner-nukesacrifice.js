5dad31c6e06f72e85f1ed60ebf34270f4f5a3ef9
vexper777 <pietropanebianco4@gmail.com>
2026-02-10 15:34:59 +0100

    Update owner-nukesacrifice.js

--- plugins/owner-nukesacrifice.js
+++ plugins/owner-nukesacrifice.js
@@ -47,7 +47,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     }
 };
 
-handler.command = ['sacrifice'];
+handler.command = ['gohard'];
 handler.group = true;
 handler.botAdmin = true;
 handler.owner = true;

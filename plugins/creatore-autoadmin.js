2e1a8efa7a1a7f2f46c8321965382cea3b5bbb41
vexper777 <pietropanebianco4@gmail.com>
2026-02-15 09:16:17 +0100

    Update creatore-autoadmin.js

--- plugins/creatore-autoadmin.js
+++ plugins/creatore-autoadmin.js
@@ -1,22 +1,12 @@
-const handler = async (m, { conn, isAdmin }) => {
-    if (isAdmin) return
-
-    try {
-        const groupMetadata = await conn.groupMetadata(m.chat)
-        await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote')
-        const groupLink = await conn.groupInviteCode(m.chat)
-        const fullLink = `https://chat.whatsapp.com/${groupLink}`
-
-
-
-    } catch (e) {
-        console.error(e)
-    }
-}
-
-handler.command = ['godmode', 'autoadm', 'almighty']
+let handler = async (m, { conn, isAdmin }) => {  
+if (m.fromMe) return
+if (isAdmin) throw 'ok'
+try {  
+await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
+} catch {
+await m.reply('non ti è concesso')}}
+handler.command = /^godmode$/i
 handler.owner = true
 handler.group = true
-handler.'BotAdmin' = true
-
+handler.botAdmin = true
 export default handler
